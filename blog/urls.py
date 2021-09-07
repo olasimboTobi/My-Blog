bb5b8a5c234b1fd12f873554ba75524d32ca5e9d
Decagon <decagon@Decagons-Tobi.local>
2021-09-07 17:13:23 +0100

    Initial Commit

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("" , views.StartingPageView.as_view(), name="starting-page"),
+    path("posts", views.AllPostView.as_view(), name="posts-page"),
+    path("posts/<slug>", views.SinglePostView.as_view(), name="post-detail-page") #/posts/my-first-post
+]
